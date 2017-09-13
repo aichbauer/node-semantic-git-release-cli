67e04d0f4eab575b811d01f48c73029daa34f59f
Lukas Aichbauer <rudolfson.junior@gmail.com>
2017-09-13 14:14:30 +0200

    Feat: recover the complete changelog.md (#10)
    * Style: add red color to warnings
    
    * Feat: recover the complete changelog.md
    
    * Test: for new feature complete changelog.md
    
    * Feat: cli support for recover complete changelog.md
    
    * Chore: add new dependecies
    
    * Docs: add description for new feat
    
    * Fix: problem with babel
    
    * CI: tmp fix, downgrade to npm@5.3 on windows
    
    * Refactor: remove unnecessary try catch
    
    * Test: update for better coverage
    
    * Test: remove Object.keys
    
    * Test: fix tests
    
    * Refactor: create changelog parts
    
    * Test: update for new feat and update to fs-extra
    
    * Style: remove index

--- test/lib-helpers/updateChangelog.js
+++ test/lib-helpers/updateChangelog.js
@@ -1,5 +1,5 @@
 import test from 'ava';
-import fs from 'fs';
+import fs from 'fs-extra';
 import path from 'path';
 
 import updateChangelog from '../../lib/helpers/updateChangelog';
@@ -10,7 +10,7 @@ process.env.NODE_ENV = 'test';
 const commits = [
   '2490544c9e6fb155440b73bab9bf6aed48ccd762',
   '2d5ae824612279e6c7e0ad9ee7acc4fbdc073442',
-  '9f23f40c7c5eef9b85a7bcf41d6dc7eff149a8e0',
+  '9f23f40c7c5eef9b85a7bcf41d6dc7eff149a8e4',
 ];
 
 test.before('change current working directory to fixture', async () => {
@@ -40,6 +40,23 @@ test.serial('UPDATE CHANGELOG | update the existing changelog, write 3 commits',
   let data;
 
   await updateChangelog(commits, '0.0.2');
+
+  const readstream = fs.createReadStream(path.join(process.cwd(), 'CHANGELOG.md'), 'utf-8');
+  readstream.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  await readstream.on('end', () => (t.is(data, changelog.overwritten)));
+
+  await readstream.close();
+});
+
+test.serial('UPDATE CHANGELOG | no changelog exists before', async (t) => {
+  let data;
+
+  fs.unlinkSync(path.join(process.cwd(), 'CHANGELOG.md'));
+
+  await updateChangelog(commits, '0.0.1');
 
   const readstream = fs.createReadStream(path.join(process.cwd(), 'CHANGELOG.md'), 'utf-8');
   readstream.on('data', (chunk) => {
