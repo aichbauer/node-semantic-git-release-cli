43bb3e96436295b73bc6205436ae3b123cfa8f06
Lukas Aichbauer <rudolfson.junior@gmail.com>
2017-10-23 21:55:53 +0200

    Feat: change from command options to option flags (#17)
    * Feat: change change from command options to option flags
    
    * Test: update for new feature

---
+++ test/options/backup.js
@@ -0,0 +1,13 @@
+import test from 'ava';
+
+import backup from '../../lib/options/backup';
+
+test('OPTIONS | BACKUP | check alias, describe and type', (t) => {
+  const expected = {
+    alias: 'b',
+    describe: 'backup CHANGELOG.md when recover',
+    type: 'boolean',
+  };
+
+  t.deepEqual(backup, expected);
+});
