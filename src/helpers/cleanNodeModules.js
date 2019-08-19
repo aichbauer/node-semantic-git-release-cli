e17db8e527a3b9fb2d1891bf508a0aed27f01b4e
Lukas Aichbauer <l.aichbauer@gmail.com>
2019-08-19 09:34:58 +0200

    Fix: actually call hasYarn

--- src/helpers/cleanNodeModules.js
+++ src/helpers/cleanNodeModules.js
@@ -1,22 +1,20 @@
-import execa from 'execa';
-import fs from 'fs-extra';
-import path from 'path';
+import execa from "execa";
+import hasYarn from "has-yarn";
+import fs from "fs-extra";
+import path from "path";
 
 const deleteNodeModules = () => {
   const cwd = process.cwd();
 
-  return fs.remove(path.join(cwd, 'node_modules'));
+  return fs.remove(path.join(cwd, "node_modules"));
 };
 
-const installNodeModules = (hasYarn) => {
-  if (hasYarn) {
-    return execa.shell('yarn');
+const installNodeModules = () => {
+  if (hasYarn()) {
+    return execa.shell("yarn");
   }
 
-  return execa.shell('npm i');
+  return execa.shell("npm i");
 };
 
-export {
-  deleteNodeModules,
-  installNodeModules,
-};
+export { deleteNodeModules, installNodeModules };
