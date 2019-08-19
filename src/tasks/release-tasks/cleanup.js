fd39c963559557747466bf68098141d66a47b64a
Lukas Aichbauer <l.aichbauer@gmail.com>
2019-08-19 09:48:58 +0200

    Fix: actually call hasYarn (#33)

--- src/tasks/release-tasks/cleanup.js
+++ src/tasks/release-tasks/cleanup.js
@@ -1,35 +1,33 @@
-import Listr from 'listr';
-import hasYarn from 'has-yarn';
-import fs from 'fs-extra';
-import path from 'path';
+import Listr from "listr";
+import fs from "fs-extra";
+import path from "path";
 import {
   deleteNodeModules,
-  installNodeModules,
-} from '../../helpers/cleanNodeModules';
+  installNodeModules
+} from "../../helpers/cleanNodeModules";
 
 const cleanup = () => {
   const cwd = process.cwd();
   return new Listr([
     {
-      title: 'check package.json',
-      task: () => (
-        fs
-          .pathExists(path.join(cwd, 'package.json'))
-          .then((exists) => {
-            if (!exists) {
-              throw new Error('Error: no package.json found... make sure you are in the correct directory');
-            }
-          })
-      ),
+      title: "check package.json",
+      task: () =>
+        fs.pathExists(path.join(cwd, "package.json")).then(exists => {
+          if (!exists) {
+            throw new Error(
+              "Error: no package.json found... make sure you are in the correct directory"
+            );
+          }
+        })
     },
     {
-      title: 'delete node_modules',
-      task: () => deleteNodeModules(),
+      title: "delete node_modules",
+      task: () => deleteNodeModules()
     },
     {
-      title: 'install dependencies',
-      task: () => installNodeModules(hasYarn),
-    },
+      title: "install dependencies",
+      task: () => installNodeModules()
+    }
   ]);
 };
 
